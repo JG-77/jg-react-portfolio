e8c24e021b50fdf2610a406ff92a42c5524f1526
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-29 18:29:41 -0700

    set up header.js

--- jg-react-portfolio/src/components/Header.js
+++ jg-react-portfolio/src/components/Header.js
@@ -1,1 +1,18 @@
 //element set up
+import React from 'react';
+
+const styles = {
+  headerStyle: {
+    backgroungImage: `url('')`,
+  },
+};
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Jessie Guadarrama</h1>
+    </header>
+  );
+}
+
+export default Header;
--- jg-react-portfolio/src/components/Project.js
+++ jg-react-portfolio/src/components/Project.js
@@ -1,1 +1,2 @@
 //This component will display main content of the page
+import React, { useState } from 'react';
