24a03faa78c5435b2e179060bafcc3e00d9786dc
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-29 19:58:06 -0700

    created styles folder for styling and moved images back to public folder'

--- jg-react-portfolio/src/components/Header.js
+++ jg-react-portfolio/src/components/Header.js
@@ -1,16 +1,21 @@
 //element set up
 import React from 'react';
-
+import '../styles/Header.css';
 const styles = {
   headerStyle: {
-    backgroungImage: `url(../images/blue-skyline.jpeg)`,
+    backgroungImage: 'url(./images/blue-skyline.jpeg)',
+  },
+  headingOne: {
+    fontFamily: 'Helvitica',
   },
 };
 
 function Header() {
   return (
     <header className="header" style={styles.headerStyle}>
-      <h1>Jessie Guadarrama</h1>
+      <h1 className="heading" style={styles.headingOne}>
+        Jessie Guadarrama
+      </h1>
     </header>
   );
 }
