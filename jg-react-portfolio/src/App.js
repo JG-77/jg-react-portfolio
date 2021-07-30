a35d349271e7c853dc6517480ede4da97b981a9c
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-29 18:16:21 -0700

    set up App.js and added comments to components

---
+++ jg-react-portfolio/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Project from './components/Project';
+// import Header from './components/Header';
+// import Navigation from './components/Navigation';
+// import Footer from './components/Footer';
+
+const App = () => <Project />;
+export default App;
---
+++ jg-react-portfolio/src/components/Project.js
@@ -0,0 +1,1 @@
+//This component will display main content of the page
