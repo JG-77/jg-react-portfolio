759f91f3ed110e9313e3a32de5a64842359f5573
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-29 21:00:11 -0700

    set up Nav component

--- jg-react-portfolio/src/components/Navigation.js
+++ jg-react-portfolio/src/components/Navigation.js
@@ -1,1 +1,23 @@
 //Navbar element set up
+import React from 'react';
+
+function Navigation() {
+  return (
+    <ul>
+      <li>
+        <a href="#About">About Me</a>
+      </li>
+      <li>
+        <a href="#Portfolio">Portfolio</a>
+      </li>
+      <li>
+        <a href="#Resume">Resume</a>
+      </li>
+      <li>
+        <a href="#Contact">Contact</a>
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
