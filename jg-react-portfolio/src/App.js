453b7f8c276030c4d2de1edb71cd3e15955197e0
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-29 22:32:27 -0700

    setting up all js files in components

--- jg-react-portfolio/src/App.js
+++ jg-react-portfolio/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import PortfolioContent from './components/Project';
-// import Header from './components/Header';
-// import Navigation from './components/Navigation';
-// import Footer from './components/Footer';
 
 const App = () => <PortfolioContent />;
 export default App;
