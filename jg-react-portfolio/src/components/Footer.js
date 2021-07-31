967d70b24778ae31962f15fdd429b2e21d5db309
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-30 20:22:19 -0700

    added contacts in footer

--- jg-react-portfolio/src/components/Footer.js
+++ jg-react-portfolio/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const styles = {
   footing: {
-    height: '50px',
+    height: '100px',
     width: '100%',
     backgroundColor: 'black',
     lineHeight: '60px',
@@ -12,13 +12,47 @@ const styles = {
     textAlign: 'center',
     color: 'white',
   },
+  ul: {
+    listStyleType: 'none',
+    display: 'inline-flex',
+    flexWrap: 'flex',
+  },
+  anchor: {
+    marginRight: '40px',
+    color: 'rgb(0, 255, 213)',
+    textDecoration: 'none',
+  },
 };
 
 export default function Footer() {
   return (
     <div>
       <footer style={styles.footing}>
-        <h2 style={styles.footerHeading}>Footer</h2>
+        <h2 style={styles.footerHeading}>
+          <ul style={styles.ul}>
+            <li>
+              <a
+                style={styles.anchor}
+                href="https://www.linkedin.com/in/jessieguadarrama7/"
+              >
+                Linkedin
+              </a>
+            </li>
+            <li>
+              <a
+                style={styles.anchor}
+                href="https://www.instagram.com/ese_cocho_jes/"
+              >
+                Instagram
+              </a>
+            </li>
+            <li>
+              <a style={styles.anchor} href="https://github.com/JG-77">
+                Github
+              </a>
+            </li>
+          </ul>
+        </h2>
       </footer>
     </div>
   );
