c4388b804e0cd4e755ad9ffb25eed1a6f38eff1a
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-31 09:47:39 -0700

    set div section for resume page

--- jg-react-portfolio/src/components/pages/Resume.js
+++ jg-react-portfolio/src/components/pages/Resume.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import resume from '../../assets/Professional-Resume.pdf';
 
+const styles = {
+  divSect: {
+    background: 'rgba(0, 0, 0, 0.7)',
+    border: 'solid',
+    margin: '0px 20px 0px',
+    fontFamily: 'arial',
+    height: '450px',
+    color: 'white',
+  },
+  h2: {
+    textAlign: 'center',
+    fontSize: '30px',
+  },
+  h3: {
+    marginLeft: '10px',
+    fontSize: '18px',
+  },
+};
 export default function Resume() {
   return (
-    <div>
-      <h2>Resume</h2>
-      <h3>
+    <div style={styles.divSect}>
+      <h2 style={styles.h2}>Resume</h2>
+      <h3 style={styles.h3}>
         {`Download My `}
         <a href={resume} download="Professional-Resume.pdf" target="_blank">
           Resume
