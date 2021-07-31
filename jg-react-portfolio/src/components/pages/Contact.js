cb8ea1ddbfec8597f3edaaba738b7e124702c3c7
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-30 19:44:08 -0700

    basic styling to contact html

--- jg-react-portfolio/src/components/pages/Contact.js
+++ jg-react-portfolio/src/components/pages/Contact.js
@@ -1,26 +1,43 @@
 import React from 'react';
 
+const styles = {
+  contactHead: {
+    textAlign: 'center',
+  },
+  divSect: {
+    fontFamily: 'arial',
+  },
+  form: {
+    textAlign: 'start',
+  },
+  messageInput: {
+    height: '100px',
+    width: '250px',
+    marginBottom: '20px',
+  },
+  inputs: {
+    marginBottom: '20px',
+  },
+};
+
 export default function Contact() {
   return (
-    <div>
-      <h2>Contact</h2>
+    <div style={styles.divSect}>
+      <h2 style={styles.contactHead}>Contact</h2>
       <form
+        style={styles.form}
         action="mailto:jesguadarrama98@gmail.com"
         method="post"
         enctype="text/plain"
       >
         <label for="name">Name:</label> <br />
-        <input type="text" name="name" /> <br />
+        <input type="text" name="name" style={styles.inputs} /> <br />
         <label for="email">Email:</label> <br />
-        <input type="email" name="email" /> <br />
+        <input type="email" name="email" style={styles.inputs} /> <br />
         <label for="message">Message:</label> <br />
-        <input type="text" name="message" /> <br />
+        <input type="text" name="message" style={styles.messageInput} /> <br />
         <input type="submit" value="Submit" />
       </form>
-      {/* 
-  
-  <input type="submit" value="Submit">
-</form>  */}
     </div>
   );
 }
