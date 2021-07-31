260443095bfc94c29df527c437e35369d9e081cc
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-30 19:02:01 -0700

    set up contact from html

--- jg-react-portfolio/src/components/pages/Contact.js
+++ jg-react-portfolio/src/components/pages/Contact.js
@@ -4,6 +4,23 @@ export default function Contact() {
   return (
     <div>
       <h2>Contact</h2>
+      <form
+        action="mailto:jesguadarrama98@gmail.com"
+        method="post"
+        enctype="text/plain"
+      >
+        <label for="name">Name:</label> <br />
+        <input type="text" name="name" /> <br />
+        <label for="email">Email:</label> <br />
+        <input type="email" name="email" /> <br />
+        <label for="message">Message:</label> <br />
+        <input type="text" name="message" /> <br />
+        <input type="submit" value="Submit" />
+      </form>
+      {/* 
+  
+  <input type="submit" value="Submit">
+</form>  */}
     </div>
   );
 }
