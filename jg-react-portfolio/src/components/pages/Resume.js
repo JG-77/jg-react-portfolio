2b59344aab2f7cf6fc048285209ae935c9a348a9
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-07-30 20:02:41 -0700

    set up downloadable pdf but not responding

--- jg-react-portfolio/src/components/pages/Resume.js
+++ jg-react-portfolio/src/components/pages/Resume.js
@@ -4,6 +4,17 @@ export default function Resume() {
   return (
     <div>
       <h2>Resume</h2>
+      <h3>
+        {`Download My `}
+        {/* file not working */}
+        <a
+          href="./assets/Professional-Resume.pdf"
+          download="Professional-Resume.pdf"
+          target="_blank"
+        >
+          Resume
+        </a>
+      </h3>
     </div>
   );
 }
