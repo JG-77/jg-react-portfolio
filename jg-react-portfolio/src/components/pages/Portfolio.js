b55c4676053cff0c6f615f65c8f9e3ac6f65d41b
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-08-03 21:50:46 -0700

    restructured card fivs so that images are the background

--- jg-react-portfolio/src/components/pages/Portfolio.js
+++ jg-react-portfolio/src/components/pages/Portfolio.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../../styles/Portfolio.css';
 import fitnessNuts from '../../images/Fitness-Nuts.png';
 import noteTaker from '../../images/Note-Taker-App.png';
-import teamProfile from '../../images/Team Profile Gen.png';
+import teamProfile from '../../images/Team-Profile-Gen.png';
 import budgetTracker from '../../images/budget-tracker-app.png';
 import fitnessTracker from '../../images/Fitness-tracker.png';
 import movieBuffs from '../../images/Movie-Buffs.png';
@@ -16,46 +16,83 @@ const styles = {
     color: 'white',
     flexWrap: 'wrap',
     display: 'inline-block',
+    width: '96%',
   },
   h2: {
     textAlign: 'center',
     fontSize: '30px',
   },
-  // cardRight: {
-  //   border: 'solid',
-  //   margin: '0px 20px 10px 10px',
-  //   color: 'white',
-  //   width: '45%',
-  //   textAlign: 'center',
-  //   display: 'inline-block',
-  //   float: 'right',
-  //   backgroundColor: '#062018',
-  // },
   card: {
     border: 'solid',
-    margin: '0px 20px 10px 20px',
+    margin: '0px 20px 20px 20px',
     color: 'white',
     width: '45%',
     display: 'inline-block',
     minHeight: '250px',
+    backgroundImage: `url(${fitnessNuts})`,
+    backgroundSize: 'cover',
   },
-  cardRight: {
+  card2: {
     border: 'solid',
-    margin: '0px 20px 10px 20px',
+    margin: '0px 20px 20px 20px',
     color: 'white',
     width: '45%',
     display: 'inline-block',
     float: 'right',
     minHeight: '250px',
+    backgroundImage: `url(${noteTaker})`,
+    backgroundSize: 'cover',
   },
-  cardimg: {
-    height: '250px',
-    maxWidth: '100%',
-    display: 'flex',
-    flexWrap: 'wrap',
-    objectFill: 'fill',
-    width: '100%',
+  card3: {
+    border: 'solid',
+    margin: '0px 20px 20px 20px',
+    color: 'white',
+    width: '45%',
+    display: 'inline-block',
+    minHeight: '250px',
+    backgroundImage: `url(${teamProfile})`,
+    backgroundSize: 'cover',
   },
+  card4: {
+    border: 'solid',
+    margin: '0px 20px 20px 20px',
+    color: 'white',
+    width: '45%',
+    display: 'inline-block',
+    float: 'right',
+    minHeight: '250px',
+    backgroundImage: `url(${budgetTracker})`,
+    backgroundSize: 'cover',
+  },
+  card5: {
+    border: 'solid',
+    margin: '0px 20px 20px 20px',
+    color: 'white',
+    width: '45%',
+    display: 'inline-block',
+    minHeight: '250px',
+    backgroundImage: `url(${fitnessTracker})`,
+    backgroundSize: 'cover',
+  },
+  card6: {
+    border: 'solid',
+    margin: '0px 20px 20px 20px',
+    color: 'white',
+    width: '45%',
+    display: 'inline-block',
+    float: 'right',
+    minHeight: '250px',
+    backgroundImage: `url(${movieBuffs})`,
+    backgroundSize: 'cover',
+  },
+  // cardimg: {
+  //   height: '250px',
+  //   maxWidth: '100%',
+  //   display: 'flex',
+  //   flexWrap: 'wrap',
+  //   objectFill: 'fill',
+  //   width: '100%',
+  // },
   pTag: {
     textAlign: 'justify',
     margin: '0px 10px 0px',
@@ -67,6 +104,15 @@ const styles = {
     color: 'rgb(0, 255, 213)',
     textDecoration: 'none',
   },
+  imgg: {
+    backgroundImage: `url(${fitnessNuts})`,
+    height: '250px',
+    maxWidth: '100%',
+    display: 'flex',
+    flexWrap: 'wrap',
+    objectFill: 'fill',
+    width: '100%',
+  },
 };
 
 export default function Portfolio() {
@@ -75,29 +121,17 @@ export default function Portfolio() {
       <h2 style={styles.h2}>Portfolio</h2>
 
       {/* app 1 */}
-      <div style={styles.card}>
-        <img src={fitnessNuts} style={styles.cardimg} />
-      </div>
+      <div style={styles.card}></div>
       {/* app2 */}
-      <div style={styles.cardRight}>
-        <img src={noteTaker} style={styles.cardimg} />
-      </div>
+      <div style={styles.card2}></div>
       {/* app 3 */}
-      <div style={styles.card}>
-        <img src={teamProfile} style={styles.cardimg} />
-      </div>
+      <div style={styles.card3}></div>
       {/* app 4 */}
-      <div style={styles.cardRight}>
-        <img src={budgetTracker} style={styles.cardimg} />
-      </div>
+      <div style={styles.card4}></div>
       {/* app 5 */}
-      <div style={styles.card}>
-        <img src={fitnessTracker} style={styles.cardimg} />
-      </div>
+      <div style={styles.card5}></div>
       {/* app 6 */}
-      <div style={styles.cardRight}>
-        <img src={movieBuffs} style={styles.cardimg} />
-      </div>
+      <div style={styles.card6}></div>
     </div>
   );
 }
