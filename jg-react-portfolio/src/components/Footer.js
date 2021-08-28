3ea3f44e1cb95696349758f6f75b09bd95a88481
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-08-27 20:46:47 -0700

    added icons for profiles

--- jg-react-portfolio/src/components/Footer.js
+++ jg-react-portfolio/src/components/Footer.js
@@ -1,14 +1,17 @@
 //page footer
 import React from 'react';
 import '../styles/Footer.css';
+import stackoverFlow from '../icons/stackIcon.png';
+import gitIcon from '../icons/git-icon.png';
+import linkedIn from '../icons/linkedIn.png';
 
 const styles = {
   footing: {
-    height: '100px',
+    //height: '100px',
     width: '100%',
     backgroundColor: 'black',
-    lineHeight: '60px',
-    paddingBottom: '10px',
+    //lineHeight: '60px',
+    paddingBottom: '45px',
   },
   footerHeading: {
     textAlign: 'center',
@@ -16,13 +19,18 @@ const styles = {
   },
   ul: {
     listStyleType: 'none',
-    display: 'inline-flex',
+    //display: 'inline-flex',
     flexWrap: 'flex',
   },
   anchor: {
     marginRight: '75px',
     color: 'rgb(0, 255, 213)',
     textDecoration: 'none',
+  },
+  icon: {
+    height: '100px',
+    width: '100px',
+    marginTop: '5px',
   },
 };
 
@@ -31,13 +39,13 @@ export default function Footer() {
     <div>
       <footer style={styles.footing} className="footer">
         <h2 style={styles.footerHeading}>
-          <ul style={styles.ul}>
+          <ul style={styles.ul} className="ul">
             <li>
               <a
                 style={styles.anchor}
                 href="https://www.linkedin.com/in/jessieguadarrama7/"
               >
-                LinkedIn
+                <img src={linkedIn} style={styles.icon} alt="LinkedIn icon" />
               </a>
             </li>
             <li>
@@ -45,12 +53,16 @@ export default function Footer() {
                 style={styles.anchor}
                 href="https://www.instagram.com/ese_cocho_jes/"
               >
-                Instagram
+                <img
+                  src={stackoverFlow}
+                  style={styles.icon}
+                  alt="Stack Overflow icon"
+                />
               </a>
             </li>
             <li>
               <a style={styles.anchor} href="https://github.com/JG-77">
-                Github
+                <img src={gitIcon} style={styles.icon} alt="GitHub icon" />
               </a>
             </li>
           </ul>
